40a143e08e87ac125bed515443edbdf317131100
Filip <filip.marttala@gmail.com>
2023-06-15 15:44:26 +0200

    let's try this instead because i can't figure out how the hell to import non-tiny matlab matrices ¯\_(ツ)_/¯

--- tests/test_parallel.cpp
+++ tests/test_parallel.cpp
@@ -12,35 +12,30 @@ int main(int argc, char **argv) {
 
     // Create a 2 by 2 matrix
     Mat A;
-    MatCreate(PETSC_COMM_WORLD, &A);
-    MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, 400000, 400000);
+    PetscInt Asz = 2*2*7*8*9;
+    MatCreateAIJ(PETSC_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,Asz,Asz,3,NULL, 4,NULL,Mat *A);
+    MatSetRandom(A,NULL);
     MatSetFromOptions(A);
     MatSetUp(A);
 
+    // Assemble the matrix
+    MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
+
+    // Create a Asz by 1 vector
     Vec b;
     VecCreate(PETSC_COMM_WORLD, &b);
-
-    PetscViewer viewer;
-    PetscViewerCreate(PETSC_COMM_WORLD,&viewer);
-    PetscViewerSetType(viewer,PETSCVIEWERHDF5);
-    PetscViewerFileSetMode(viewer,FILE_MODE_READ);
-
-    PetscViewerFileSetName(viewer,"testmats/Arandsparse.mat");
+    VecSetSizes(b, PETSC_DECIDE, Asz);
+    VecSetFromOptions(b);
+    VecSetRandom(b, NULL);
+    VecAssemblyBegin(b);
+    VecAssemblyEnd(b);
 
     // Make a scaling value
     PetscReal t = -1;
 
     PetscObjectSetName((PetscObject)A, "A");
     PetscObjectSetName((PetscObject)b, "b");
-
-    MatLoad(A, viewer);
-    VecLoad(b, viewer);
-
-    MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
-    MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
-
-    VecAssemblyBegin(b);
-    VecAssemblyEnd(b);
 
     expmv matexp(t, A, b);
     
